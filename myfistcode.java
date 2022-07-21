477f73bc41a5acf9796b682dd7220ef3e3ae0130
Morakot Choetkiertikul <morakotch@gmail.com>
2022-07-21 10:43:51 +0700

    Update myfistcode.java

--- myfistcode.java
+++ myfistcode.java
@@ -1,3 +1,9 @@
-This is my first code in Java.
+// This is my first code in Java.
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
 
 
